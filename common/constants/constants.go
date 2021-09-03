e906612453905a59f2c28541f2b5d42994e2fb74
Hari Krishnan <harikrishnan83@gmail.com>
2021-09-03 21:47:18 +0530

    updating version

--- common/constants/constants.go
+++ common/constants/constants.go
@@ -15,7 +15,7 @@ const (
 	DOCKER_MINOR_VERSION         = 10
 	DOCKER_COMPOSE_MAJOR_VERSION = 1
 	DOCKER_COMPOSE_MINOR_VERSION = 29
-	PERFIZ_CLI_VERSION           = "0.0.18"
+	PERFIZ_CLI_VERSION           = "0.0.19"
 
 	SKIP_TEMPLATE_MESSAGE = " is already present. Skipping."
 )
